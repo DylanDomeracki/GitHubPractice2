bdb84e8d62b73967c8533708f99d3f5b7b2fd9b9
Dylan Domeracki <ddomeracki4103@Stu.d214.org>
2023-04-06 14:02:25 -0500

    Initial Commit

---
+++ GitHubPractice2/GitHubPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPractice2App.swift
+//  GitHubPractice2
+//
+//  Created by Dylan Domeracki on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
